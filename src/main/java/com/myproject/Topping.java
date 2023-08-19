0d77696552e7e44530d1c4a8e0daecbe46dae4c1
isaacflores2205@gmail.com <isaacflores2205@gmail.com>
2023-08-18 21:39:58 -0600

    Initial commit

---
+++ src/main/java/com/myproject/Topping.java
@@ -0,0 +1,19 @@
+package edu.ejercicios;
+
+
+    // Clase para representar un topping de pizza
+    public class Topping {
+        private String nombre;
+
+        // Constructor de la clase Topping
+        public Topping(String nombre) {
+            this.nombre = nombre;
+        }
+
+        // Método para agregar el topping a una pizza
+        public void agregar(Pizza pizza) {
+            pizza.getIngredientes().add(nombre);
+        }
+    }
+
+
